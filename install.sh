a01abc69562a07a2eb21c350d40c1320c59ae7a1
PRIYANK RAI <0priyankrai@gmail.com>
2022-08-03 12:58:03 +0530

    bash zsh configuration

--- install.sh
+++ install.sh
@@ -85,8 +85,6 @@ function clean() {
 function install_zsh() {
     sudo nala install zsh -y;
     cd "$LOCAL_HOME" || echo "unable to cd in home dir"; 
-    # ohmyzsh installation 
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && exit;
     # ohmyzsh autosuggestion plugin 
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
     # ohmyzsh syntax higlight 
@@ -113,7 +111,7 @@ function git-setup () {
     stow fonts
 
 
-    sudo chsh -s $(which zsh) $USER;
+    
 }
 
 
@@ -133,3 +131,6 @@ function main() {
 }
 
 main;
+# ohmyzsh installation 
+sudo chsh -s $(which zsh) $USER;
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
