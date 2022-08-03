b46238a961e76b4c40cc18cfbc8e143156e83654
PRIYANK RAI <0priyankrai@gmail.com>
2022-08-03 11:53:33 +0530

    bash zsh configuration

--- install.sh
+++ install.sh
@@ -82,9 +82,29 @@ function clean() {
 }
 
 
+function install_zsh() {
+    sudo nala install zsh -y;
+    cd "$LOCAL_HOME" || echo "unable to cd in home dir"; 
+    # ohmyzsh installation 
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+    # ohmyzsh autosuggestion plugin 
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
+    # ohmyzsh syntax higlight 
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    
+
+    echo "Changing default shell to zsh ";
+    chsh -s $(which zsh);
+    
+    echo ~;
+    rm -rf ~/.zshrc || echo "zshrc not remove might be not present "
+    rm -rf ~/.bashrc || echo "bashrc not  remove might not exist initially"
+
+}
+
 function git-setup () {
      echo "insisde git-setup function "; 
-    /bin/bash "$LOCAL_HOME/.dotfiles/install_zsh.sh";
+    install_zsh;
     cd "$LOCAL_HOME" 
     cd .dotfiles 
     stow bash 
