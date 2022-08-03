44ce8800a0d281a1121afff56a577d3464ddd871
PRIYANK RAI <0priyankrai@gmail.com>
2022-08-03 13:26:35 +0530

    final setup .

--- install.sh
+++ install.sh
@@ -88,11 +88,11 @@ function install_zsh() {
     # ohmyzsh installation 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
     # ohmyzsh autosuggestion plugin 
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
+    git clone https://github.com/zsh-users/zsh-autosuggestions;
     # ohmyzsh syntax higlight 
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git;
     # ohmyzsh powerlevel10k 
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git;
 
     echo "Changing default shell to zsh ";
     
