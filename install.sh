31fa4dcd5e31b4a37e8a2b13c4719e11d186c291
PRIYANK RAI <0priyankrai@gmail.com>
2022-08-03 13:12:46 +0530

    bash zsh configuration

--- install.sh
+++ install.sh
@@ -86,13 +86,13 @@ function install_zsh() {
     sudo nala install zsh -y;
     cd "$LOCAL_HOME" || echo "unable to cd in home dir"; 
     # ohmyzsh installation 
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"< exit;
     # ohmyzsh autosuggestion plugin 
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
     # ohmyzsh syntax higlight 
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     # ohmyzsh powerlevel10k 
-    git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
     echo "Changing default shell to zsh ";
     
